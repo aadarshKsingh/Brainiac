a3a3d5246d303edd93bb2bbc8e82456b9e5c547e
aadarshksingh <aadarshkumarsingh198@gmail.com>
2024-01-29 18:12:56 +0530

    Fix books

--- src/Pages/Books.js
+++ src/Pages/Books.js
@@ -1,15 +1,40 @@
 import { React, useState, useEffect } from 'react'
 import Sidebar from '../Components/Sidebar'
-import { Box, TextField, Toolbar } from '@mui/material'
+import { Box, TextField, Toolbar, Snackbar } from '@mui/material'
 import { NavLink } from 'react-router-dom';
 import { useSessionStorage } from 'react-storage-complete';
 import { useNavigate } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import { Fragment } from 'react';
 export const Books = () => {
 
     const [searchText, setSearchText] = useState('');
     const [searchResults, setSearchResults] = useState('');
     const [accessToken] = useSessionStorage('access_token', '');
     const navigate = useNavigate();
+    const [open, setOpen] = useState(false);
+
+    const handleClick = () => {
+        setOpen(true);
+    };
+
+    const handleClose = (reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpen(false);
+    };
+
+    const action = (
+        <Fragment>
+            <Button color="primary" size="small" onClick={handleClose}>
+                close
+            </Button>
+            <IconButton onClick={handleClose}>
+            </IconButton>
+        </Fragment>
+    );
     useEffect(() => {
         if (!accessToken) {
             navigate("/login-student");
@@ -19,7 +44,6 @@ export const Books = () => {
     const handleSearch = () => {
         fetch('https://brainiac-mcgk.onrender.com/books?q=' + searchText)
             .then(response => {
-                console.log('Response status code:', response.status);
                 return response.json();
             })
             .then(data => {
@@ -29,6 +53,25 @@ export const Books = () => {
                 console.error('Error fetching data:', error);
             });
     };
+    const handleDownload = (isbn) => {
+        fetch('https://brainiac-mcgk.onrender.com/book?isbn=' + isbn)
+            .then(response => {
+                console.log('Response status code:', response.status);
+                return response.json();
+            })
+            .then(data => {
+                if (data === false) {
+                    handleClick();
+                }
+                if (data) {
+                    window.open(data.url, '_blank', 'noopener,noreferrer');
+                    return;
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }
 
     if (accessToken) {
         return (
@@ -60,6 +103,13 @@ export const Books = () => {
                                         Search
                                     </p>
                                 </NavLink>
+                                <Snackbar
+                                    open={open}
+                                    autoHideDuration={3000}
+                                    onClose={handleClose}
+                                    message="Book not Available"
+                                    action={action}
+                                />
                             </form>
                         </div>
                         <ul>
@@ -69,22 +119,26 @@ export const Books = () => {
                                 <li key={book.isbn}>
 
                                     <div className='flex flex-row card rounded-lg shadow-md py-3 px-5 justify-start align-middle text-left'>
-                                        <img className='w-32 h-48 justify-center align-center m-5' src={book.cover} alt={book.name} />
+                                        <div className='flex flex-col justify-center align-center  pl-24'>
+                                            <img className='w-32 h-48 relative justify-center align-center' src={book.cover} alt={book.name} />
+                                            <p className='btn bg-[#4361EE] w-32 my-2' onClick={() => handleDownload(book.isbn)}>Download</p>
+                                        </div>
                                         <div className='flex flex-col'>
-                                            <p className='text-lg font-bold'>{book.name}</p>
+                                            <p className='text-lg font-bold ml-24'>{book.name}</p>
 
-                                            <p><b>Author:</b> {book.author}</p>
+                                            <p className='ml-24'><b>Author:</b> {book.author}</p>
 
-                                            <p><b>Publish Year:</b> {book.publish_year}</p>
+                                            <p className='ml-24'><b>Publish Year:</b> {book.publish_year}</p>
 
-                                            <p><b>Publisher:</b> {book.publisher}</p>
+                                            <p className='ml-24'><b>Publisher:</b> {book.publisher}</p>
 
-                                            <p><b>Description:</b> {book.description}</p>
+                                            <p className='ml-24 line-clamp-6'><b>Description:</b> {book.description}</p>
                                         </div>
                                     </div>
                                 </li>
                             ))}
                         </ul>
+
                     </div>
                 </Box>
             </Box>
